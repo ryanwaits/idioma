3843118f6955d39add09be7c52883ea6baddbb11
Ryan Waits <ryan.waits@gmail.com>
2025-08-11 15:53:48 -0500

    cleanup

--- test_translatable.js
+++
@@ -1,8 +0,0 @@
-import { XmlStrategy } from './src/strategies/xml.js';
-
-const strategy = new XmlStrategy();
-
-// Test isTranslatableString through the strategy
-console.log('Is "16GB" translatable:', strategy.isTranslatableString('16GB', 'ram'));
-console.log('Is "Intel i7" translatable:', strategy.isTranslatableString('Intel i7', 'cpu'));
-console.log('Is "High-performance laptop" translatable:', strategy.isTranslatableString('High-performance laptop', 'description'));
